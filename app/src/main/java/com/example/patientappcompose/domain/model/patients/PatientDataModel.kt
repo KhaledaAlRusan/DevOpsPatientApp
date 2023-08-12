893878d2117c7511bf4865545d8ac50a344661b6
Khaled <khaled66658032@gmail.com>
2023-08-12 02:38:51 -0400

    Fixes api usage

--- app/src/main/java/com/example/patientappcompose/domain/model/patients/PatientDataModel.kt
+++ app/src/main/java/com/example/patientappcompose/domain/model/patients/PatientDataModel.kt
@@ -1,43 +1,60 @@
 package com.example.patientappcompose.domain.model.patients
 
-
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
+/**
+ * Data model representing a patient's details.
+ */
 @JsonClass(generateAdapter = true)
 data class PatientDataModel(
-    @Json(name ="address")
-    val address: String ,
-    @Json(name ="birthdate")
-    val birthdate: String ,
-    @Json(name = "condition")
-    val condition: String ,
-    @Json(name = "createdAt")
-    val createdAt: String ,
-    @Json(name = "email")
-    val email: String ,
-    @Json(name = "gender")
-    val gender: String ,
     @Json(name = "_id")
-    val id: String ,
-    @Json(name = "mobile")
-    val mobile: String ,
-    @Json(name = "name")
-    val name: String ,
-    @Json(name = "photo")
-    val photo: String ,
-    @Json(name = "tests")
-    val tests: List<Any> ,
-    @Json(name = "updatedAt")
-    val updatedAt: String ,
-    @Json(name = "__v")
-    val v: Int ,
-    //Local var
+    val id: String,
 
-    var selected:Boolean =false,
-){
-    fun getPatientInfo():String{
-        return "lives in $address \nEmail: $email \nBorn on $birthdate"
+    @Json(name = "name")
+    val name: String,
+
+    @Json(name = "email")
+    val email: String,
+
+    @Json(name = "mobile")
+    val mobile: String,
+
+    @Json(name = "gender")
+    val gender: String,
+
+    @Json(name = "birthdate")
+    val birthdate: String,
+
+    @Json(name = "address")
+    val address: String,
+
+    @Json(name = "photo")
+    val photo: String,
+
+    @Json(name = "condition")
+    val condition: String,
+
+    @Json(name = "tests")
+    val tests: List<Any>,
+
+    @Json(name = "createdAt")
+    val createdAt: String,
+
+    @Json(name = "updatedAt")
+    val updatedAt: String,
+
+    @Json(name = "__v")
+    val v: Int,
+
+    // Local variable to keep track of selection state
+    var selected: Boolean = false
+) {
+    /**
+     * Generates a concise patient information string.
+     * @return String with patient's address, email, and birthdate.
+     */
+    fun getPatientInfo(): String {
+        return "Lives in $address \nEmail: $email \nBorn on $birthdate"
     }
 }
-
